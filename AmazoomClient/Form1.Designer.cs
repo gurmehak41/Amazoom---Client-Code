0016be929971ee0c02b3a369dba1808e7dfa474f
Gurmehak Pannu <gm.pannu41@gmail.com>
2021-12-05 00:16:22 -0800

    Added Functionality to display order Status as well as put unique identifier in order placements

--- AmazoomClient/Form1.Designer.cs
+++ AmazoomClient/Form1.Designer.cs
@@ -47,8 +47,11 @@ namespace AmazoomClient
 			this.label7 = new System.Windows.Forms.Label();
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.dataGridViewOrderStatus = new System.Windows.Forms.DataGridView();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrderStatus)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -174,6 +177,11 @@ namespace AmazoomClient
 			this.buttonConnectServer.UseVisualStyleBackColor = true;
 			this.buttonConnectServer.Click += new System.EventHandler(this.buttonConnectServer_Click);
 			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
 			// label7
 			// 
 			this.label7.AutoSize = true;
@@ -202,11 +210,33 @@ namespace AmazoomClient
 			this.pictureBox2.TabIndex = 15;
 			this.pictureBox2.TabStop = false;
 			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(32, 312);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(129, 25);
+			this.label8.TabIndex = 16;
+			this.label8.Text = "Order Status:";
+			// 
+			// dataGridViewOrderStatus
+			// 
+			this.dataGridViewOrderStatus.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewOrderStatus.Location = new System.Drawing.Point(28, 340);
+			this.dataGridViewOrderStatus.Name = "dataGridViewOrderStatus";
+			this.dataGridViewOrderStatus.RowHeadersWidth = 51;
+			this.dataGridViewOrderStatus.RowTemplate.Height = 24;
+			this.dataGridViewOrderStatus.Size = new System.Drawing.Size(404, 174);
+			this.dataGridViewOrderStatus.TabIndex = 17;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(638, 539);
+			this.Controls.Add(this.dataGridViewOrderStatus);
+			this.Controls.Add(this.label8);
 			this.Controls.Add(this.label7);
 			this.Controls.Add(this.buttonConnectServer);
 			this.Controls.Add(this.textBoxClientID);
@@ -228,6 +258,7 @@ namespace AmazoomClient
 			this.Load += new System.EventHandler(this.Form1_Load);
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrderStatus)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -252,6 +283,8 @@ namespace AmazoomClient
 		private System.Windows.Forms.Label label7;
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.DataGridView dataGridViewOrderStatus;
 	}
 }
 
--- AmazoomClient/Form1.cs
+++ AmazoomClient/Form1.cs
@@ -13,9 +13,12 @@ using System.Collections.Concurrent;
 
 namespace AmazoomClient
 {
+
     public partial class Form1 : Form
     {
         private  SimpleTcpClient client;
+        private List<orderInfo> orderList = new List<orderInfo>();
+        int orderCounter = 0;
 
         public Form1()
         {
@@ -29,7 +32,27 @@ namespace AmazoomClient
 
         private void DataReceived(object sender, SimpleTCP.Message e)
         {
-            Debug.WriteLine($"Data received from server: {e.MessageString}");
+            string message = e.MessageString;
+            string[] splitMessage;
+            int identifier;
+
+            splitMessage = message.Split('/');
+            identifier = Convert.ToInt32(splitMessage[2]);
+
+            //Check if identifier already exists
+            int i = 0;
+            foreach (orderInfo order in orderList)
+            {
+                if ((identifier / 1000 == order.ClientId) && (identifier % 1000 == order.OrderId))
+                {
+                    orderList[i].Status = splitMessage[3];
+                    return;
+
+                }
+                i++;
+            }
+
+            orderList.Add(new orderInfo(identifier, splitMessage[3]));
         }
 
         private void buttonConnectServer_Click(object sender, EventArgs e)
@@ -57,9 +80,12 @@ namespace AmazoomClient
 		private void buttonPlaceOrder_Click(object sender, EventArgs e)
 		{
             string message = "";
+            int identifier = Convert.ToInt32(textBoxClientID.Text) * 1000 + orderCounter;
+
+            orderCounter++;
 
             //Build up message
-            message = "OrderFromClient/" + textBoxClientID.Text + "/-/";
+            message = "OrderFromClient/" + Convert.ToString(identifier) + "/-/";
 
             if (textBoxQtyApples.Text != "")
                 message += "Apple*" + textBoxQtyApples.Text + ",";
@@ -68,6 +94,32 @@ namespace AmazoomClient
                 message += "Orange*" + textBoxQtyOrange.Text + ",";
 
             this.client.WriteLine(message);
+
+        }
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+            dataGridViewOrderStatus.DataSource = null;  // need this to work
+            dataGridViewOrderStatus.DataSource = this.orderList;
         }
 	}
+
+	public class orderInfo
+    {
+        public int ClientId
+        { get; }
+
+        public int OrderId
+        { get; }
+
+        public string Status
+        { get; set; }
+
+        public orderInfo(int combinedId, string status)
+        {
+            this.ClientId = combinedId / 1000;
+            this.OrderId = combinedId % 1000;
+            this.Status = status;
+        }
+    }
 }
